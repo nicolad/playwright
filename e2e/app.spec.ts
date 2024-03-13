c8089f6d7d4da38affe42845407075ccb424beda
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2024-03-13 17:21:04 +0200

    Small improvement.

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -5,6 +5,7 @@ test("should navigate to the about page", async ({ page }) => {
 
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("/");
+
   // Find an element with the text 'About' and click on it
   await page.click("text=About");
   // The new URL should be "/about" (baseURL is used there)
