c900cfe3d40d8e6701c79059d8de2fa686fa0adb
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2024-03-13 16:52:13 +0200

    Small improvement.

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 test("should navigate to the about page", async ({ page }) => {
+  await page.screenshot({ path: "screenshot.png" });
+
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("/");
   // Find an element with the text 'About' and click on it
